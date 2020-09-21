6d60361b127ef83524c0cd267c5bf7186ab0a0c0
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-09-20 22:26:28 -0300

    referencia do bootstrap apartir do node

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,6 @@
 let button = document.querySelector('form button.btn');
 button.addEventListener('click', () => {
   //prettier-ignore
-  //location.href = "../views/deshboard.html"
+  location.href = "../views/deshboard.html";
   alert('callback');
 });
