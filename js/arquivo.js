485a8ec6eb719d40f2aa4642adf89bdeea594a2d
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-09-22 19:01:23 -0300

    mudança da mensagem do alert

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,6 @@
-let button = document.querySelector('form button.btn');
+let button = document.querySelector('#btnLogin');
 button.addEventListener('click', () => {
   //prettier-ignore
   location.href = "../views/deshboard.html";
-  alert('callback');
+  alert('cliquei no botão');
 });
