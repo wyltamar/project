9068f73083299fcb3b57d60a1dbc5a21a49b539c
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-09-22 21:19:26 -0300

    retirada do alert para teste - resolvido

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,6 +1,5 @@
 let button = document.querySelector('#btnLogin');
 button.addEventListener('click', () => {
   //prettier-ignore
-  location.href = "../views/dashboard.html";
-  alert('cliquei no botão');
+  location.href= "../views/dashboard.html";
 });
