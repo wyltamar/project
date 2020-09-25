cbf0f852c41eaeb4e050faedfb53293286a6c021
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-09-24 23:55:30 -0300

    dinamizando a função loadDoc

--- js/serverPages.js
+++ js/serverPages.js
@@ -1,4 +1,4 @@
-function loadDoc() {
+function loadDoc(url) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
@@ -6,6 +6,6 @@ function loadDoc() {
     }
   };
 
-  xhttp.open('GET', 'client.html', true);
+  xhttp.open('GET', url, true);
   xhttp.send();
 }
