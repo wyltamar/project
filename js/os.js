e87ca15b7d626ce133c4d78151bdde11e53d1df6
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2020-11-03 00:31:49 -0300

    Inicio do mapeamento dos campos da os e manipulação dos dados

--- js/os.js
+++ js/os.js
@@ -1,1 +1,34 @@
-console.log('Integração realizada com sucesso!');
+function InserirOS() {
+  var situacaoOs = document.getElementById('situacaoOs').value;
+  var checkOs = document.getElementsByClassName('check-os')[0].checked;
+  var checkOrcamento = document.getElementsByClassName('check-orcamento')[0]
+    .checked;
+  switch (situacaoOs) {
+    case '1':
+      situacaoOs = 'Selecione';
+      break;
+    case '2':
+      situacaoOs = 'Aguardando execução';
+      break;
+    case '3':
+      situacaoOs = 'Executada';
+      break;
+    case '4':
+      situacaoOs = 'Na bancada';
+      break;
+    default:
+      situacaoOs = 'Aguardando peças';
+      break;
+  }
+
+  var tipo = '';
+  checkOs ? (tipo = 'OS') : (tipo = 'Orçamento');
+
+  console.log(tipo);
+}
+function marcaDesmarca(caller) {
+  var checks = document.querySelectorAll('input[type="checkbox"]');
+  for (let i = 0; i < checks.length; i++) {
+    checks[i].checked = checks[i] == caller;
+  }
+}
