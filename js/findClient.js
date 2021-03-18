b8c3be55dbfd35a5fba8f4d57407d1f417f73e70
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-03-17 21:00:46 -0300

    customer search by id

--- js/findClient.js
+++ js/findClient.js
@@ -1,11 +1,9 @@
-// buscar cliente pelo 'id'
-var id = document.getElementById('id');
 var idCliente;
 
-id.addEventListener('blur', () => {
-  idCliente = id.value;
-  console.log(idCliente);
-});
+function pegarId() {
+  let id = document.getElementById('id').value;
+  idCliente = id;
+}
 
 function buscarCliente() {
   const options = {
@@ -21,23 +19,18 @@ function buscarCliente() {
     })
     .then((data) => {
       for (const dado in data) {
-        console.log('dentro do for');
-        console.log(data[dado]);
-        console.log('imprimiu os dados');
-
         if (idCliente == data[dado].id) {
-          console.log('Entrou no if');
           document.getElementById('id').value = data[dado].id;
           document.getElementById('nome').value = data[dado].nome;
           document.getElementById('endereco').value = data[dado].endereco;
           document.getElementById('telefone').value = data[dado].telefone;
           document.getElementById('email').value = data[dado].email;
+
+          console.log('Consulta bem sucedida!');
         }
       }
     })
-    .catch((Error) => {
-      console.log('Error');
-    });
+    .catch(Error);
 }
 
 buscarCliente();
