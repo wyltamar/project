b8b2f2327c0eeec9126c5c4eec356256823a3092
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2021-05-10 21:41:43 -0300

    Desenvolmimento da função inserir OS (fontend)

--- js/os.js
+++ js/os.js
@@ -39,7 +39,47 @@ function InserirOS() {
   console.log(servico);
   console.log(tecnico);
   console.log(valor);
+
+  //prettier-ignore
+  var os = {
+    tipo: tipo,
+    situacao: situacaoOs,
+    idCli: idCliente,
+    equipamento: equipamento,
+    defeito: defeito,
+    servico: servico,
+    tecnico: tecnico,
+    valor: valor
+  };
+  const url = `http://localhost:3000/os`;
+
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById('box-content').innerHTML = this.responseText;
+    }
+  };
+
+  xhttp.open('POST', url, true);
+  xhttp.setRequestHeader('Content-Type', 'application/json');
+  xhttp.send(JSON.stringify(os));
+
+  limpaCampos();
 }
+function limpaCampos() {
+  var situacaoOs = (document.getElementById('situacaoOs').value = '');
+  var cliente = (document.getElementById('cliente').value = '');
+  var idCliente = (document.getElementById('idCliente').value = '');
+  var equipamento = (document.getElementById('equipamento').value = '');
+  var defeito = (document.getElementById('defeito').value = '');
+  var servico = (document.getElementById('servico').value = '');
+  var tecnico = (document.getElementById('tecnico').value = '');
+  var valor = (document.getElementById('valor').value = '');
+  var checkOs = (document.getElementsByClassName(
+    'check-os'
+  )[0].checked = false);
+}
+
 function marcaDesmarca(caller) {
   var checks = document.querySelectorAll('input[type="checkbox"]');
   for (let i = 0; i < checks.length; i++) {
